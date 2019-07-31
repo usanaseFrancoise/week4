e9bf1d62593047e0bda90389fbff1715b07ea311
usanase Francoise <fusanasencoise@gmail.com>
2019-07-31 15:38:47 +0200

    jquerry

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $(".click1").click(function() {
+        $("#form").show();
+        $("#a").show ();  
+          
+    });
+    $(".click2").click(function() {
+        $("#form").show();
+        $("#b").show ();  
+});
+$(".click3").click(function() {
+    $("#form").show();
+    $("#c").show ();  
+}); $(".click4").click(function() {
+    $("#form").show();
+    $("#d").show ();  
+});
+});
