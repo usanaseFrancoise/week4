7bb5f5e9b879b57feba5d326587eed8cb8863755
usanase Francoise <fusanasencoise@gmail.com>
2019-08-02 15:16:33 +0200

    {

--- js/index.js
+++ js/index.js
@@ -1,18 +1,111 @@
 $(document).ready(function() {
-    $(".click1").click(function() {
+    $("#ordern").click(function() {
         $("#form").show();
-        $("#a").show ();  
+        $("#ordern").show ();  
           
     });
-    $(".click2").click(function() {
+    $("#ordern").click(function() {
         $("#form").show();
-        $("#b").show ();  
+        $("#ordern").show ();  
 });
-$(".click3").click(function() {
+$(".#ordern").click(function() {
     $("#form").show();
-    $("#c").show ();  
-}); $(".click4").click(function() {
+    $("#ordern").show ();  
+}); $("#ordern").click(function() {
     $("#form").show();
-    $("#d").show ();  
+    $("#ordern").show ();  
 });
 });
+$(document).ready(function() {
+    $("#add").click(function() {
+    $("#checked1").append('<div id="checked1">' +
+            '<h2>pizza Form</h2>' +
+            '<div class="label">'+
+                'Name'+
+            '</div>'+
+                        '<input id="id-name" type="text" size="50">'+
+
+                    '<div class="label">'+
+                        'Address'+
+                   ' </div>'+
+                    '<input id="id-address"  type="text" size="50">'+
+                           
+                           '<div class="label">'+
+                                'Phone'+
+                           ' </div>'+
+                   '<input id="id-phone" type="text" size="10">'+
+                           
+                          ' <div id="choose">'+
+                            'choose'+
+                          ' </div>'+
+                           '<select>'+
+                            '<option value="choose Pizza">select Pizza</option>'+
+                            '<option value="PEPERONI pizza">PEPERONI pizza</option>'+
+                            '<option value="PIZERIA pizza">PIZERIA pizza</option>'+
+                            '<option value="CHICKEN pizza">CHICKEN pizza</option>'+
+                            '<option value="DEEP FISH pizza">DEEP FISH pizza</option>'+
+                          '</select>'+
+                          '<div id="size">'+
+                            'Pizza size'+
+                           '</div>'+
+                           '<select>'+
+                            '<option value=" Pizza size">  Select Size</option>'+
+                            '<option value="SMALL"> SMALL             </option>'+
+                            '<option value="MEDIUM">MEDIUM</option>'+
+                            '<option value="BIG ">BIG </option>'+
+                          '</select>'+
+                        
+                    '<div class="group">'+
+                        '<div class="title">'+
+                            'Crust'+
+                        '</div>'+
+                        '<div class="label">'+
+                           ' <input id="id-crust"  name="crust" type="radio" value="Thin">'+
+                                   
+                                    'Thin'+                                 
+                        '</div>'+
+                        '<div class="label">'+
+                                '<input id="id-crust" name="crust" type="radio" value="Classic">'+
+                                        'Classic'+
+                            '</div>'+
+                            '<div class="label">'+
+                                   ' <input id="id-crust" name="crust" type="radio" value="Deep Dish">'+
+                                           ' Deep Dish'+
+                                '</div>'+
+                    '</div>'+
+                    '<div class="group">'+
+                        '<div class="title">'+
+                            'Toppings'+
+                       ' </div>'+
+                        '<div class="label">'+
+                                    '<input  name="topping" type="checkbox" value="Sausage">'+
+                                     'Sausage'+
+                        '</div>'+
+                        '<div class="label">'+
+                                '<input name="topping" type="checkbox" value="Mushrooms">'+
+                                        ' Mushrooms'+
+                            '</div>'+
+                           '<div class="label">'+
+                                   ' <input name="topping" type="checkbox" value="Onions">'+
+                                             'Onions'+
+                                '</div>'+
+                                '<div class="label">'+
+                                        '<input name="topping" type="checkbox" value="Green peppers">'+
+                                                 'Gree peppers'+
+                                   ' </div>'+
+                                   ' <div class="label">'+
+                                            '<input name="topping" type="checkbox" value="Black olives">'+
+                                                     'Black olives'+
+                                        '</div>'+
+                                        '<div class="label">'+
+                                                '<input name="topping" type="checkbox" value="Extra chees">'+
+                                                        ' Extra chees'+
+                                            '</div>'+
+                                        '</div>'+
+                                        '<div class="label">'+
+                                            'Pizza Number'+
+                                        '</div>'+
+                                        '<input id="id-number" type="number" min="1"class="form-control">'+
+                                  '</div>');
+    });
+    });
